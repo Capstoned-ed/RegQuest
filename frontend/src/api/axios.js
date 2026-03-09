062ebbc86b69c3771c538fec8919fdeef9de3600
nacht1234 <ralphperoch1789@gmail.com>
2026-03-09 19:41:31 +0800

    fix backend baseurl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://regquest-backend.onrender.com',
+    baseURL: 'https://regquest-backend.onrender.com/api',
 });
 
 api.interceptors.request.use(
